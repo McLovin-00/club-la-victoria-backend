fe02fad8952a2b32f0c0c3572c88cfe2291c3356
AgustinAlbonico <agustin_010@live.com>
2025-12-04 11:10:08 -0300

    Agregue que el synchronize de typeorm se active o no segun si la app esta en produccion o no, esto lo hice con una variable de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,7 +53,7 @@ import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
           Usuario,
           RegistroIngreso,
         ],
-        synchronize: false,
+        synchronize: configService.getNodeEnv() === 'development',
       }),
     }),
     CloudinaryModule,
